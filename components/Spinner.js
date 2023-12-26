1bf800f57410de59c98c4e98d175a16db96f6c07
Duy Truong <truongbaoduy2003@gmail.com>
2023-12-26 11:13:47 +0700

    tiny tiny tiny fix spinner

--- components/Spinner.js
+++ components/Spinner.js
@@ -4,7 +4,7 @@ export default function Spinner({ fullWidth }) {
   if (fullWidth) {
     return (
       <div className="w-full flex justify-center">
-        <PulseLoader color={"#1E3A8A"} speedMultiplier={2} />;
+        <PulseLoader color={"#1E3A8A"} speedMultiplier={2} />
       </div>
     );
   }
